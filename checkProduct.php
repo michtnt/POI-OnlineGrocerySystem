1739eaf3263b1510ec766748fadbc23d867ae8d7
Ubuntu <ubuntu@ip-172-31-7-254.ec2.internal>
2021-04-03 08:55:41 +0000

    added checkout, more logic to check product, etc

--- checkProduct.php
+++ checkProduct.php
@@ -3,14 +3,22 @@
 $connection = mysqli_connect('localhost', 'uts', 'internet', 'assignment1');
 $data = file_get_contents( "php://input" ); //$data is now the string '[1,2,3]';
 
-$data = json_decode( $data ); //$data is now a php array array(1,2,3)
-
-// TODO:
+$data = json_decode( $data );
+$result_array = array();
 // validate to stock
-// all good: move to payment page
-// no good: send alert 
+for ($x = 0; $x < sizeof($data); $x++) {
+    // TODO validate against total item bought not 0
+  $query_string = "select * from products where product_id = ".$data[$x]." and in_stock > 0";
+  $result = mysqli_query($connection, $query_string);
+  $num_rows = mysqli_num_rows($result);
+  if($num_rows == 0){ // no good: send alert 
+      $result_array[$data[$x]] = false;
+  } else {
+      $result_array[$data[$x]] = true;
+  }
+}
 
 //close the connection
 mysqli_close($connection);
-
+echo json_encode($result_array);
 ?>
